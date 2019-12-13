5f857a8ce6ec7c99709e5ee668106714690bea75
Nikheel Neelay <P10496648@capita.co.uk>
2019-12-13 21:55:03 +0530

    Added new proj

---
+++ RealTimeCharts_Server/RealTimeServer/HubConfig/ChartHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace RealTimeServer.HubConfig
+{
+    public class ChartHub : Hub
+    {
+        public async Task BroadcastChartData(string data) => await Clients.All.SendAsync("broadcastchartdata", new Random().Next().ToString());
+    }
+}
